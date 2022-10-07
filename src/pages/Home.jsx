a4e17a7f7ac7ae5afc377323177cefa14c14b9d9
Radojica Popovc <108525632+radojicapopovic@users.noreply.github.com>
2022-10-07 23:44:32 +0200

    e-commerce-app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Announcment from '../components/Announcment'
+import Categories from '../components/Categories'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+
+const Home = () => {
+  return (
+    <div>
+        <Announcment/>
+        <Navbar/>
+        <Slider/>
+        <Categories/>
+    </div>
+  )
+}
+
+export default Home
